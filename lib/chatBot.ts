91987bd91f2ef2165d0cc0ebbd24df311ac925ed
Abhijeet Kadam <kadamabhi1881@gmail.com>
2025-04-05 11:48:18 +0530

    minor chage

--- lib/chatBot.ts
+++ lib/chatBot.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 export async function sendMessageToAI(message: string): Promise<string> {
   try {
-    // const response = await axios.post(`${process.env.NEXT_PUBLIC_SITE_URL}/api/chat`, { message });
-    const response = await axios.post(`/api/chat`, { message });
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_SITE_URL}/api/chat`, { message });
+    // const response = await axios.post(`/api/chat`, { message });
     console.log("Frontend AI RESPONSE: ",response.data)
     return response.data.reply;
   } catch (error) {
